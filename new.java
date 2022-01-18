84f6892390593c8f0c6db2f7a626596b9cf37396
Satyaa Goyal <satgoyal@publicisgroupe.net>
2022-01-18 11:40:13 +0530

    2nd Commit

---
+++ new.java
@@ -0,0 +1,15 @@
+package com.satya;
+
+import javax.sound.sampled.SourceDataLine;
+
+class Satya {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        NewFunctionForTest("This is function");
+    }
+
+    public static void NewFunctionForTest(String x){
+        System.out.println(x);
+        System.out.println("New line");
+    }
+}
